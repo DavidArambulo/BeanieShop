b33bf686bfafaba3362a0c350c8cc72db78d5b1a
David Arambulo <davida.arambuloa@gmail.com>
2021-07-02 13:47:05 -0300

    Estructura HTML del NavBar component

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import NavBar from './components/Navbar/navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar/navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const NavBar = () => {
+    return (
+        <nav>
+            <h1>BeanieShop</h1>
+            <ul>
+                <li>Catálogo</li>
+                <li>Seguimiento</li>
+                <li>Contacto</li>
+            </ul>
+        </nav>
+    );
+}
+
+export default NavBar;
