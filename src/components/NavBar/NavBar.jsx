2a44f1d86378ab8d2b4e4d489e0df6d84f88eef0
David Arambulo <davida.arambuloa@gmail.com>
2021-07-31 21:29:19 -0300

    se agrego animacion de aparicion del CartWidget

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,28 @@
+import React, { useContext } from "react";
+import CartWidget from "../CartWidget/CartWidget";
+import { Link } from "react-router-dom";
+import { categorias } from "../../data/categorias.json";
+import { CartContext } from "../../services/CartContext";
+
+const NavBar = () => {
+    const { cantTotal } = useContext(CartContext);
+    return (
+        <header className="contenedor">
+            <nav className="navbar">
+                <h1 className="navbar-logo"><Link to={'/'}>BeanieShop</Link></h1>
+                <ul className="navbar-menu">
+                    <li className="menu-item"><Link to={'/'}>Catálogo</Link></li>
+                    {categorias.map(categoria => (
+                            <li className="menu-item" key={categoria.id}>
+                                <Link to={`/categoria/${categoria.id}`}>{categoria.nombre}</Link>
+                            </li>
+                    ))}
+                    <li className="menu-item"><Link to={'/seguimiento'}>Seguimiento</Link></li>
+                    {cantTotal > 0 && <li className="menu-item"><Link to={'/cart'}><CartWidget /></Link></li>}
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default NavBar;
